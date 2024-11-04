fb15c38733b20d20b7480e473a22c7e72142661f
Ripat Al Safar <56125965+Ripat-2345@users.noreply.github.com>
2024-11-04 13:19:12 +0700

    docs: create folder structure MVVM
    empty folder structure MVVM

--- app/src/main/java/com/sayid/studypath/ui/view/MainActivity.kt
+++ app/src/main/java/com/sayid/studypath/ui/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.sayid.studypath
+package com.sayid.studypath.ui.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.sayid.studypath.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/sayid/studypath/ui/viewmodel/ViewModelFactory.kt
@@ -0,0 +1,4 @@
+package com.sayid.studypath.ui.viewmodel
+
+class ViewModelFactory {
+}
