4589778d98a693566c63ca0f22d1ebc119f2622f
SayidAchmad <syaidahmad7z@gmail.com>
2024-11-10 15:30:31 +0700

    Add Onboarding Screen Layout

---
+++ app/src/main/java/com/sayid/studypath/ui/activity/OnboardingActivity.kt
@@ -0,0 +1,93 @@
+package com.sayid.studypath.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.sayid.studypath.R
+import com.sayid.studypath.databinding.ActivityOnboardingBinding
+import com.sayid.studypath.ui.adapter.OnboardingPageAdapter
+
+class OnboardingActivity : AppCompatActivity() {
+    @Suppress("ktlint:standard:backing-property-naming")
+    private var _binding: ActivityOnboardingBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initializeOnboardingPage()
+
+        binding.btnOnboarding.setOnClickListener {
+            if (binding.viewPager.currentItem < (binding.viewPager.adapter?.itemCount ?: 0) - 1) {
+                binding.viewPager.currentItem += 1
+            } else {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+    private fun initializeOnboardingPage() {
+        val illustrations =
+            listOf(
+                R.drawable.undraw_onboarding_welcoming,
+                R.drawable.undraw_onboarding_exams,
+                R.drawable.undraw_onboarding_mobile_app,
+                R.drawable.undraw_onboarding_chat_bot,
+                R.drawable.undraw_onboarding_certification,
+            )
+        val personalityData =
+            mapOf(
+                getString(R.string.onboarding_title_1) to getString(R.string.onboarding_desc_1),
+                getString(R.string.onboarding_title_2) to getString(R.string.onboarding_desc_2),
+                getString(R.string.onboarding_title_3) to getString(R.string.onboarding_desc_3),
+                getString(R.string.onboarding_title_4) to getString(R.string.onboarding_desc_4),
+                getString(R.string.onboarding_title_5) to getString(R.string.onboarding_desc_5),
+            )
+
+        val viewPager = binding.viewPager
+        val adapter =
+            OnboardingPageAdapter(
+                this,
+                illustrations,
+                personalityData,
+            )
+        viewPager.adapter = adapter
+
+        viewPager.registerOnPageChangeCallback(
+            object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    updateIndicator(position)
+                    binding.btnOnboarding.apply {
+                        when (position) {
+                            0 -> text = getString(R.string.start)
+                            1, 2, 3 -> text = getString(R.string.next)
+                            else -> text = getString(R.string.enter)
+                        }
+                    }
+                }
+            },
+        )
+    }
+
+    private fun updateIndicator(currentPosition: Int) {
+        val indicatorLayout = binding.indicatorLayout
+        for (i in 0 until indicatorLayout.childCount) {
+            val view = indicatorLayout.getChildAt(i)
+            if (i == currentPosition) {
+                view.setBackgroundResource(R.drawable.indicator_active)
+            } else {
+                view.setBackgroundResource(R.drawable.indicator_inactive)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
