5caa267a1407723cb976e1fa09ea8ed813350a1a
SayidAchmad <syaidahmad7z@gmail.com>
2024-11-19 14:12:34 +0700

    Update Text Appearance Style for Onboarding Screen and Other Screens

--- app/src/main/java/com/sayid/studypath/ui/fragment/StatisticsFragment.kt
+++ app/src/main/java/com/sayid/studypath/ui/fragment/StatisticsFragment.kt
@@ -24,57 +24,66 @@ class StatisticsFragment : Fragment() {
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
         super.onViewCreated(view, savedInstanceState)
         val bigFivePieChartsView = binding.bigFivePieCharts
-        val bigFiveRadarChartsView = binding.bigFiveRadarCharts
 
-        val bigFivePieChartsModel: AAChartModel = AAChartModel()
-            .chartType(AAChartType.Pie)
-            .dataLabelsEnabled(true)
-            .backgroundColor("#00000000")
-            .colorsTheme(arrayOf("#16C79A", "#CBB53B", "#11698E", "#EE7D52", "AC56C1"))
-            .series(
-                arrayOf(
-                    AASeriesElement()
-                        .name("Big Five Personality")
-                        .size("100%")
-                        .data(
-                            arrayOf(
-                                arrayOf("Openness", 0.56315989),
-                                arrayOf("Conscientiousness", 0.71610975),
-                                arrayOf("Neuroticism", 0.64705192),
-                                arrayOf("Extroversion", 0.35946574),
-                                arrayOf("Agreeableness", 0.58775013)
-                            )
-                        )
+        val bigFivePieChartsModel: AAChartModel =
+            AAChartModel()
+                .chartType(AAChartType.Pie)
+                .dataLabelsEnabled(true)
+                .backgroundColor("#00000000")
+                .colorsTheme(arrayOf("#16C79A", "#CBB53B", "#11698E", "#EE7D52", "AC56C1"))
+                .series(
+                    arrayOf(
+                        AASeriesElement()
+                            .name("Big Five Personality")
+                            .size("100%")
+                            .data(
+                                arrayOf(
+                                    arrayOf("Openness", 0.56315989),
+                                    arrayOf("Conscientiousness", 0.71610975),
+                                    arrayOf("Neuroticism", 0.64705192),
+                                    arrayOf("Extroversion", 0.35946574),
+                                    arrayOf("Agreeableness", 0.58775013),
+                                ),
+                            ),
+                    ),
                 )
-            )
 
-        val bigFiveRadarChartsModel: AAChartModel = AAChartModel()
-            .chartType(AAChartType.Area)
-            .backgroundColor("#00000000")
-            .categories(
-                arrayOf(
-                    "Openness",
-                    "Conscientiousness",
-                    "Neuroticism",
-                    "Extroversion",
-                    "Agreeableness"
-                )
-            )
-            .colorsTheme(arrayOf("#16C79A"))
-            .series(
-                arrayOf(
-                    AASeriesElement()
-                        .name("Score")
-                        .data(arrayOf(0.56315989, 0.71610975, 0.64705192, 0.35946574, 0.58775013))
-                )
-            )
-            .polar(true)
+        val bigFiveRadarChartsModel: AAChartModel =
+            AAChartModel()
+                .chartType(AAChartType.Area)
+                .backgroundColor("#00000000")
+                .categories(
+                    arrayOf(
+                        "Openness",
+                        "Conscientiousness",
+                        "Neuroticism",
+                        "Extroversion",
+                        "Agreeableness",
+                    ),
+                ).colorsTheme(arrayOf("#16C79A"))
+                .series(
+                    arrayOf(
+                        AASeriesElement()
+                            .name("Score")
+                            .data(
+                                arrayOf(
+                                    0.56315989,
+                                    0.71610975,
+                                    0.64705192,
+                                    0.35946574,
+                                    0.58775013,
+                                ),
+                            ),
+                    ),
+                ).polar(true)
 
         bigFivePieChartsView.aa_drawChartWithChartModel(bigFivePieChartsModel)
-        bigFiveRadarChartsView.aa_drawChartWithChartModel(bigFiveRadarChartsModel)
     }
 
     override fun onDestroyView() {
