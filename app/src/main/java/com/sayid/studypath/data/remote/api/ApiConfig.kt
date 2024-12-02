bbc18cb1f5f64899bef1b8ba846efbba54817585
SayidAchmad <syaidahmad7z@gmail.com>
2024-12-02 18:59:37 +0700

    Adjust User Profile Size and Api Config Base Url

--- app/src/main/java/com/sayid/studypath/data/remote/api/ApiConfig.kt
+++ app/src/main/java/com/sayid/studypath/data/remote/api/ApiConfig.kt
@@ -26,7 +26,7 @@ class ApiConfig {
             val retrofit =
                 Retrofit
                     .Builder()
-                    .baseUrl("http://192.168.100.86:3000/")
+                    .baseUrl(BuildConfig.BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .client(client)
                     .build()
